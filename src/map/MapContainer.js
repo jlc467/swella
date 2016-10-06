5f144bd448bedc13c03d0fd4e68e69a2700027b7
John Collins <johnlamontcollins@gmail.com>
2016-10-05 20:33:50 -0400

    make it real

--- src/map/MapContainer.js
+++ src/map/MapContainer.js
@@ -7,58 +7,51 @@ class MapContainer extends Component {
     mapboxgl.accessToken = 'pk.eyJ1IjoiamNtdXNlIiwiYSI6ImVqMmlmeTQifQ.Z4cdYoe1Htq-9aEd5Qnjsw'
     this.map = new mapboxgl.Map({
       container: 'glmap',
-      style: 'mapbox://styles/mapbox/streets-v9'
+      style: 'mapbox://styles/jcmuse/cikcm77hi007l9fm127xym4v1'
     })
     this.map.on('load', () => {
-      // Add the source to query. In this example we're using
-      // county polygons uploaded as vector tiles
-      this.map.addSource('counties', {
+      this.map.addSource('zones', {
           "type": "vector",
-          "url": "mapbox://mapbox.82pkq93d"
+          "url": "mapbox://jcmuse.r53o9a4i"
       })
 
       this.map.addLayer({
-          "id": "counties",
+          "id": "zones",
           "type": "fill",
-          "source": "counties",
-          "source-layer": "original",
+          "source": "zones",
+          "source-layer": "zones-geometry-good",
           "paint": {
               "fill-outline-color": "rgba(0,0,0,0.1)",
               "fill-color": "rgba(0,0,0,0.1)"
           }
-      }, 'place-city-sm') // Place polygon under these labels.
+      })
 
       this.map.addLayer({
-          "id": "counties-highlighted",
+          "id": "zones-highlighted",
           "type": "fill",
-          "source": "counties",
-          "source-layer": "original",
+          "source": "zones",
+          "source-layer": "zones-geometry-good",
           "paint": {
-              "fill-outline-color": "#484896",
-              "fill-color": "#6e599f",
-              "fill-opacity": 0.75
+              "fill-outline-color": "#5FB3B3",
+              "fill-color": "#5FB3B3",
+              "fill-opacity": 1
           },
-          "filter": ["in", "COUNTY", ""]
-      }, 'place-city-sm')
+          "filter": ["in", "ID", ""]
+      })
       this.map.on('mousemove', (e) => {
         const features = this.map.queryRenderedFeatures(e.point, {
-            layers: ['counties']
+            layers: ['zones']
         })
-
         // Change the cursor style as a UI indicator.
         this.map.getCanvas().style.cursor = features.length ? 'pointer' : ''
 
-        // Remove things if no feature was found.
         if (!features.length) {
-            this.map.setFilter('counties-highlighted', ['in', 'COUNTY', ''])
+            this.map.setFilter('zones-highlighted', ['in', 'ID', ''])
             return
         }
 
-        // Single out the first found feature on mouseove.
         const feature = features[0]
-
-        // Add features that share the same county name to the highlighted layer.
-        this.map.setFilter('counties-highlighted', ['in', 'COUNTY', feature.properties.COUNTY])
+        this.map.setFilter('zones-highlighted', ['in', 'ID', feature.properties.ID])
       })
     })
   }
