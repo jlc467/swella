981768bc1f45ec82d84156c56ef79bcc86d21a1c
John Collins <johnlamontcollins@gmail.com>
2016-10-16 11:00:56 -0400

    highlight nearest zone to geocode result

--- client/src/map/MapContainer.js
+++ client/src/map/MapContainer.js
@@ -1,88 +1,98 @@
 import React, { Component } from 'react'
 import { parseJSON, checkStatus, buildJSONRequest } from '../utility'
 import { SWELLA_API_URL } from '../config'
+import has from 'lodash/has'
 // import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 // import 'mapbox-gl-geocoder/dist/'
 // import 'mapbox-gl/dist/mapbox-gl.css'
 /*global mapboxgl:true*/
 /*eslint no-undef: "error"*/
 
+const setupMap = component => {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiamNtdXNlIiwiYSI6ImVqMmlmeTQifQ.Z4cdYoe1Htq-9aEd5Qnjsw'
+  component.map = new mapboxgl.Map({
+    container: 'glmap',
+    style: 'mapbox://styles/jcmuse/ciubuehhf003l2ipixkxcllyk'
+  })
+  // disable map rotation using right click + drag
+  component.map.dragRotate.disable()
+  // disable map rotation using touch rotation gesture
+  component.map.touchZoomRotate.disableRotation()
+  // setup geocoder
+  component.geocoder = new mapboxgl.Geocoder({
+    flyTo: false,
+    zoom: false,
+    position: "top-left",
+    country: 'us',
+    types: 'region,postcode,place,locality,neighborhood,poi',
+    placeholder: 'Where will you be boating out of?'
+  })
+  component.geocoder.on('result', () => {
+    const result = component.geocoder.getResult()
+    if (has(result, 'geometry.coordinates')) {
+      component.getNearbyZones({
+        "lat": result.geometry.coordinates[1],
+        "long": result.geometry.coordinates[0]
+      })
+    }
+  })
+  component.map.on('load', () => {
 
+    component.map.addSource('zones', {
+      "type": "vector",
+      "url": "mapbox://jcmuse.r53o9a4i"
+    })
+
+    component.map.addLayer({
+      "id": "zones",
+      "type": "fill",
+      "source": "zones",
+      "source-layer": "zones-geometry-good",
+      "paint": {
+        "fill-outline-color": "#5888E8",
+        "fill-color": "#7D9FF5"
+      }
+    })
+
+    component.map.addLayer({
+      "id": "zones-highlighted",
+      "type": "fill",
+      "source": "zones",
+      "source-layer": "zones-geometry-good",
+      "paint": {
+        "fill-outline-color": "#28CDA3",
+        "fill-color": "#28CDA3",
+      },
+      "filter": ["in", "ID", ""]
+    })
+    component.map.addControl(component.geocoder)
+    component.map.on('mousemove', (e) => {
+      const features = component.map.queryRenderedFeatures(e.point, {
+        layers: ['zones']
+      })
+      // Change the cursor style as a UI indicator.
+      component.map.getCanvas().style.cursor = features.length ? 'pointer' : ''
+
+      if (!features.length) {
+        component.map.setFilter('zones-highlighted', ['in', 'ID', ''])
+        return
+      }
+
+      const feature = features[0]
+      component.map.setFilter('zones-highlighted', ['in', 'ID', feature.properties.ID])
+    })
+  })
+
+}
 
 class MapContainer extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
       nearbyZones: null
     }
   }
-  componentDidMount() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiamNtdXNlIiwiYSI6ImVqMmlmeTQifQ.Z4cdYoe1Htq-9aEd5Qnjsw'
-    this.map = new mapboxgl.Map({
-      container: 'glmap',
-      style: 'mapbox://styles/jcmuse/ciubuehhf003l2ipixkxcllyk'
-    })
-    this.geocoder = new mapboxgl.Geocoder({
-      flyTo: false,
-      zoom: false,
-      position: "top-left",
-      country: 'us',
-      placeholder: 'Where will you be boating out of?'
-    })
-    this.geocoder.on('results', e => {
-      console.log('results: ', e.results)
-    })
-    this.map.on('load', () => {
-
-      this.map.addSource('zones', {
-        "type": "vector",
-        "url": "mapbox://jcmuse.r53o9a4i"
-      })
-
-      this.map.addLayer({
-        "id": "zones",
-        "type": "fill",
-        "source": "zones",
-        "source-layer": "zones-geometry-good",
-        "paint": {
-          "fill-outline-color": "#5888E8",
-          "fill-color": "#7D9FF5"
-        }
-      })
-
-      this.map.addLayer({
-        "id": "zones-highlighted",
-        "type": "fill",
-        "source": "zones",
-        "source-layer": "zones-geometry-good",
-        "paint": {
-          "fill-outline-color": "#28CDA3",
-          "fill-color": "#28CDA3",
-        },
-        "filter": ["in", "ID", ""]
-      })
-      this.map.addControl(this.geocoder)
-      this.getNearbyZones({
-        "lat":32.02940,
-        "long":-87.70589
-      })
-      this.map.on('mousemove', (e) => {
-        const features = this.map.queryRenderedFeatures(e.point, {
-          layers: ['zones']
-        })
-        // Change the cursor style as a UI indicator.
-        this.map.getCanvas().style.cursor = features.length ? 'pointer' : ''
-
-        if (!features.length) {
-          this.map.setFilter('zones-highlighted', ['in', 'ID', ''])
-          return
-        }
-
-        const feature = features[0]
-        this.map.setFilter('zones-highlighted', ['in', 'ID', feature.properties.ID])
-      })
-    })
-  }
+  componentDidMount() {setupMap(this)}
   componentWillUpdate(nextProps, nextState) {
     if (!this.state.nearbyZones && nextState.nearbyZones) {
       const feature = nextState.nearbyZones.features[0]
