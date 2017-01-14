c355b287c7f72b8f982ddfcb421088baf019aa4b
John Collins <johnlamontcollins@gmail.com>
2017-01-14 15:16:30 -0500

    handle zoom from url

--- client/src/map/MapContainer.js
+++ client/src/map/MapContainer.js
@@ -1,192 +1,194 @@
-import React, { Component } from 'react'
-import { parseJSON, checkStatus, buildJSONRequest } from '../utility'
-import { SWELLA_API_URL } from '../config'
-import has from 'lodash/has'
+import React, {Component} from 'react';
+import {parseJSON, checkStatus, buildJSONRequest} from '../utility';
+import {SWELLA_API_URL} from '../config';
+import has from 'lodash/has';
+import createHashHistory from 'history/createHashHistory';
+const history = createHashHistory({ hashType: 'slash' });
 // import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 // import 'mapbox-gl-geocoder/dist/'
 // import 'mapbox-gl/dist/mapbox-gl.css'
 /*global mapboxgl:true*/
 /*global MapboxGeocoder:true*/
 /*eslint no-undef: "error"*/
-
-
 const setupMap = component => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiamNtdXNlIiwiYSI6ImVqMmlmeTQifQ.Z4cdYoe1Htq-9aEd5Qnjsw'
-  component.map = new mapboxgl.Map({
-    container: 'glmap',
-    style: 'mapbox://styles/jcmuse/ciwtpzev9002h2prwecbbvcg1'
-  })
+  mapboxgl.accessToken = 'pk.eyJ1IjoiamNtdXNlIiwiYSI6ImVqMmlmeTQifQ.Z4cdYoe1Htq-9aEd5Qnjsw';
+  component.map = new mapboxgl.Map({ container: 'glmap', style: 'mapbox://styles/jcmuse/ciwtpzev9002h2prwecbbvcg1' });
   // disable map rotation using right click + drag
-  component.map.dragRotate.disable()
+  component.map.dragRotate.disable();
   // disable map rotation using touch rotation gesture
-  component.map.touchZoomRotate.disableRotation()
+  component.map.touchZoomRotate.disableRotation();
   // setup geocoder
   component.geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      flyTo: false,
-      zoom: false,
-      country: 'us',
-      types: 'region,postcode,place,locality,neighborhood,poi',
-      placeholder: 'Where will you be boating out of?'
-  })
+    accessToken: mapboxgl.accessToken,
+    flyTo: false,
+    zoom: false,
+    country: 'us',
+    types: 'region,postcode,place,locality,neighborhood,poi',
+    placeholder: 'Where will you be boating out of?'
+  });
 
   component.geocoder.on('result', ({ result }) => {
-    console.log(result)
+    console.log(result);
     if (has(result, 'geometry.coordinates')) {
-      component.getNearbyZones({
-        "lat": result.geometry.coordinates[1],
-        "long": result.geometry.coordinates[0]
-      })
-      component.map.getSource('single-point').setData(result.geometry)
+      component.getNearbyZones({ 'lat': result.geometry.coordinates[1], 'long': result.geometry.coordinates[0] });
+      component.map.getSource('single-point').setData(result.geometry);
     }
-  })
+  });
   component.map.on('load', () => {
-
-    component.map.addSource('zones', {
-      "type": "vector",
-      "url": "mapbox://jcmuse.r53o9a4i"
-    })
+    component.map.addSource('zones', { 'type': 'vector', 'url': 'mapbox://jcmuse.r53o9a4i' });
 
     component.map.addLayer({
-      "id": "zones",
-      "type": "fill",
-      "source": "zones",
-      "source-layer": "zones-geometry-good",
-      "paint": {
-        "fill-outline-color": "#5888E8",
-        "fill-color": "#7D9FF5",
-        "fill-opacity": 0
-      }
-    })
+      'id': 'zones',
+      'type': 'fill',
+      'source': 'zones',
+      'source-layer': 'zones-geometry-good',
+      'paint': { 'fill-outline-color': '#5888E8', 'fill-color': '#7D9FF5', 'fill-opacity': 0 }
+    });
     component.map.addSource('single-point', {
-      "type": "geojson",
-      "data": {
-        "type": "FeatureCollection",
-        "features": []
-      }
-    })
+      'type': 'geojson',
+      'data': { 'type': 'FeatureCollection', 'features': [] }
+    });
     component.map.addLayer({
-      "id": "point",
-      "source": "single-point",
-      "type": "circle",
-      "paint": {
-        "circle-radius": 7,
-        "circle-color": "#FF8000"
-      }
-    })
+      'id': 'point',
+      'source': 'single-point',
+      'type': 'circle',
+      'paint': { 'circle-radius': 7, 'circle-color': '#FF8000' }
+    });
 
     component.map.addLayer({
-      "id": "zones-highlighted",
-      "type": "fill",
-      "source": "zones",
-      "source-layer": "zones-geometry-good",
-      "paint": {
-        "fill-outline-color": "#F2AE51",
-        "fill-color": "#FFB454",
-        "fill-opacity": .5
-      },
-      "filter": ["in", "ID", ""]
-    })
+      'id': 'zones-highlighted',
+      'type': 'fill',
+      'source': 'zones',
+      'source-layer': 'zones-geometry-good',
+      'paint': { 'fill-outline-color': '#F2AE51', 'fill-color': '#FFB454', 'fill-opacity': 0.5 },
+      'filter': [ 'in', 'ID', '' ]
+    });
 
     component.map.addLayer({
-      "id": "zones-selected",
-      "type": "fill",
-      "source": "zones",
-      "source-layer": "zones-geometry-good",
-      "paint": {
-        "fill-outline-color": "#F2AE51",
-        "fill-color": "#FFB454",
-        "fill-opacity": .5
-      },
-      "filter": ["in", "ID", ""]
-    })
-
+      'id': 'zones-selected',
+      'type': 'fill',
+      'source': 'zones',
+      'source-layer': 'zones-geometry-good',
+      'paint': { 'fill-outline-color': '#F2AE51', 'fill-color': '#FFB454', 'fill-opacity': 0.5 },
+      'filter': [ 'in', 'ID', '' ]
+    });
+    if (routeHasZoneId(history.location)) {
+      const initialParams = getParamsFromLocation(history.location);
+      component.getZoneById(initialParams.zoneId, initialParams.zoomOutFactor);
+    }
     //component.map.addControl(component.geocoder)
-    component.map.on('mousemove', (e) => {
-      const features = component.map.queryRenderedFeatures(e.point, {
-        layers: ['zones']
-      })
-      // Change the cursor style as a UI indicator.
-      component.map.getCanvas().style.cursor = features.length ? 'pointer' : ''
+    // component.map.on('mousemove', e => {
+    //   const features = component.map.queryRenderedFeatures(e.point, { layers: [ 'zones' ] });
+    //   // Change the cursor style as a UI indicator.
+    //   component.map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+    //   if (!features.length) {
+    //     component.map.setFilter('zones-highlighted', [
+    //       'in',
+    //       'ID',
+    //       component.state.activeZone && component.state.activeZone.zoneId
+    //         ? component.state.activeZone.zoneId.toUpperCase()
+    //         : ''
+    //     ]);
+    //     return;
+    //   }
+    //   const feature = features[0];
+    //   component.map.setFilter('zones-highlighted', [ 'in', 'ID', feature.properties.ID ]);
+    // });
+    component.map.on('click', e => {
+      const features = component.map.queryRenderedFeatures(e.point, { layers: [ 'zones' ] });
+      component.map.getCanvas().style.cursor = features.length ? 'pointer' : '';
 
       if (!features.length) {
-        component.map.setFilter('zones-highlighted',
-          [ 'in', 'ID', component.state.activeZone && component.state.activeZone.zoneId ? component.state.activeZone.zoneId.toUpperCase() : '' ]
-        )
-        return
+        return;
       }
 
-      const feature = features[0]
-      component.map.setFilter('zones-highlighted', ['in', 'ID', feature.properties.ID])
-    })
-    component.map.on('click', (e) => {
-      const features = component.map.queryRenderedFeatures(e.point, {
-        layers: ['zones']
-      })
-      component.map.getCanvas().style.cursor = features.length ? 'pointer' : ''
+      const feature = features[0];
+      component.getZoneById(feature.properties.ID);
+    });
+  });
+};
 
-      if (!features.length) {
-        return
-      }
+const getParamsFromLocation = location => {
+  const toReturn = {
+    zoneId: location.pathname.replace('/zoneId/', ''),
+    zoomOutFactor: location.search.indexOf('zoomOutFactor') > -1 ? location.search.replace('?zoomOutFactor=', '') : '0'
+  };
+  return toReturn;
+};
 
-      const feature = features[0]
-      component.getZoneById(feature.properties.ID)
-    })
-  })
+const routeHasZoneId = location => location.pathname.indexOf('zoneId') > -1;
 
-}
+const setupRouteListener = component => {
+  history.listen((location, action) => {
+    if (routeHasZoneId(location)) {
+      const newParams = getParamsFromLocation(location);
+      console.log(newParams);
+      component.getZoneById(newParams.zoneId, newParams.zoomOutFactor);
+    }
+  });
+};
 
 class MapContainer extends Component {
-  state = {
-    activeZone: null
+  state = { activeZone: null, zoomOutFactor: null };
+  componentDidMount() {
+    setupRouteListener(this);
+    setupMap(this);
   }
-  componentDidMount() {setupMap(this)}
-  componentWillUpdate(nextProps, nextState) {
-    if (!this.state.activeZone && nextState.activeZone) {
-      this.map.fitBounds(nextState.activeZone.bbox, { padding: 200 })
-      this.map.setFilter('zones-selected', ['in', 'ID', nextState.activeZone.zoneId.toUpperCase()])
-    }
-  }
+  handleZoneZoom = () => {
+    this.map.once('zoomend', e => {
+      this.map.once('zoomend', e => {
+        console.log('has zoom out factor', e.zoomOutFactor);
+        if (e.zoomOutFactor) {
+          const newZoomLevel = this.map.getZoom() * ((100 - e.zoomOutFactor) / 100);
+          console.log(newZoomLevel, 'newZoomLevel');
+          this.map.jumpTo({ zoom: newZoomLevel });
+        }
+      });
+      this.map.fitBounds(this.state.activeZone.bbox, { easing: () => 1, linear: false, padding: 200 }, {
+        zoomOutFactor: this.state.zoomOutFactor
+      });
+
+      this.map.setFilter('zones-selected', [ 'in', 'ID', this.state.activeZone.zoneId.toUpperCase() ]);
+    });
+    this.map.jumpTo({ zoom: this.map.getZoom() + 0.00001 }); // change bounds to force fitBounds to trigger zoomend
+  };
   getNearbyZones = coordinates => {
-    this.setState({ activeZone: null },
-      () => {
-        fetch(`${SWELLA_API_URL}/getNearbyZones`,
-          buildJSONRequest('post', { coordinates })
-        )
+    this.setState({ activeZone: null }, () => {
+      fetch(`${SWELLA_API_URL}/getNearbyZones`, buildJSONRequest('post', { coordinates }))
         .then(checkStatus)
         .then(parseJSON)
         .then(activeZone => {
-          this.setState({ activeZone })
+          this.setState({ activeZone });
         })
         .catch(error => {
-          console.log(error)
-        })
-      }
-    )
-  }
-  getZoneById = zoneId => {
-    this.setState({ activeZone: null },
-      () => {
-        fetch(`${SWELLA_API_URL}/getZoneById`,
-          buildJSONRequest('post', { zoneId })
-        )
-        .then(checkStatus)
-        .then(parseJSON)
-        .then(activeZone => {
-          this.setState({ activeZone })
-        })
-        .catch(error => {
-          console.log(error)
-        })
-      }
-    )
-  }
+          console.log(error);
+        });
+    });
+  };
+  getZoneById = (zoneId, zoomOutFactor) => {
+    if (this.state.activeZone && this.state.activeZone.zoneId === zoneId) {
+      // short circuit data call
+      this.setState({ zoomOutFactor }, () => this.handleZoneZoom());
+      return;
+    }
+    fetch(`${SWELLA_API_URL}/getZoneById`, buildJSONRequest('post', { zoneId }))
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(activeZone => {
+        console.log(activeZone, 'activeZone');
+        this.setState({ activeZone, zoomOutFactor }, () => this.handleZoneZoom());
+      })
+      .catch(error => {
+        console.log(error);
+        console.log(error);
+      });
+  };
   render() {
     return (
-      <div className="MapContainer">
-        <div id="glmap"/>
+      <div className='MapContainer'>
+        <div id='glmap' />
       </div>
-    )
+    );
   }
 }
 
